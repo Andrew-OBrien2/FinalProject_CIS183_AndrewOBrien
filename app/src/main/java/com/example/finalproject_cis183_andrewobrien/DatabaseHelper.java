95e1860450f0fc856fe3dd6754bb9cc9190673f4
OwnedCoot <aobriencollege@gmail.com>
2024-11-29 13:05:28 -0500

    First DB push

--- app/src/main/java/com/example/finalproject_cis183_andrewobrien/DatabaseHelper.java
+++ app/src/main/java/com/example/finalproject_cis183_andrewobrien/DatabaseHelper.java
@@ -1,13 +1,21 @@
 package com.example.finalproject_cis183_andrewobrien;
 
+import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
 
 public class DatabaseHelper extends SQLiteOpenHelper
 {
     private static final String database_name = "AppData.db";
     private static final String students_table_name = "Students";
     private static final String majors_table_name = "Majors";
+
+    public DatabaseHelper(Context c)
+    {
+        super(c,database_name,null,1);
+    }
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase)
